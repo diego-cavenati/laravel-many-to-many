ccc8a7810d6f8e539cec81c7a3894462a2e0997e
diego-cavenati <diegocavenati@gmail.com>
2023-01-17 14:56:57 +0100

    add model Tecnology and seeding and migrate

---
+++ database/migrations/2023_01_17_134528_create_project_tecnology_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_tecnology', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('tecnology_id');
+            $table->foreign('tecnology_id')->references('id')->on('tecnologies')->cascadeOnDelete();
+
+            $table->primary(['project_id', 'tecnology_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_tecnology');
+    }
+};
---
+++ database/seeders/TecnologySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tecnology;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TecnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tecnologies = ['html', 'css', 'laravel', 'php', 'vue.js', 'Js', 'MySql'];
+
+        foreach ($tecnologies as $tecnology) {
+            $newTecnology = new Tecnology();
+            $newTecnology->name = $tecnology;
+            $newTecnology->slug = Str::slug($tecnologies);
+            $newTecnology->save();
+        }
+    }
+}
